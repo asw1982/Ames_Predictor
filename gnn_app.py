361d67566bf2294b8b96ab9a96a9fe07510439c8
asw1982 <99703772+asw1982@users.noreply.github.com>
2023-07-26 14:09:31 +0900

    Update gnn_app.py

--- gnn_app.py
+++ gnn_app.py
@@ -26,8 +26,10 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///test.db'
 
 db = SQLAlchemy(app)
-
-
+#==================
+app.app_context().push()
+db.create_all()
+#===================
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200),nullable=False)
